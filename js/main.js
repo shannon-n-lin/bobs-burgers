3696ce781a3ce9e240564a5ff3879c1428049e9b
Shannon Lin <shannon@Shannons-MacBook-Air.local>
2023-02-14 16:36:14 -0800

    Add episode number validation

--- js/main.js
+++ js/main.js
@@ -17,14 +17,35 @@ function getStore() {
                 for (let obj in data) {
                     numSeasons.push(data[obj].season)
                 }
-                // find highest number of seasons
+                // find highest season number possible
                 maxSeasons = Math.max(...numSeasons)
-                // validate input
+                // validate user input
                 if (s <= 0 || s > maxSeasons) {
                     alert(`Please enter a season number between 1 and ${maxSeasons}.`)
                 }
             }
             validateSeasonNum()
+
+            function validateEpNum() {
+                // loop through each season number
+                for (let i = 1; i <= maxSeasons; i++) {
+                    let numEpisodes = []
+                    // create list of all episode numbers for the season
+                    for (let obj in data) {
+                        if (data[obj].season == i) {
+                            numEpisodes.push(data[obj.episode])
+                        }
+                    }
+                    // find highest episode number possible for the season
+                    let maxEpisodes = Math.max(...numEpisodes)
+                    // validate user input
+                    if (s == i && ep > maxEpisodes) {
+                        alert(`That season only has ${maxEpisodes} episodes.`)
+                    } else if (s == i && ep <= 0) {
+                        alert(`Please enter an episode number between 1 and ${maxEpisodes}.`)
+                    }
+                }
+            }
 
       })
 
