ca0dde8a593806fa8680cf0e6cd5c2e47a0321de
Shannon Lin <shannon@Shannons-MacBook-Air.local>
2023-02-14 16:30:43 -0800

    Add episode number validation

--- js/main.js
+++ js/main.js
@@ -2,11 +2,29 @@ document.getElementById('getStore').addEventListener('click', getStore)
 
 function getStore() {
     const url = 'https://bobsburgers-api.herokuapp.com/storeNextDoor/'
+    let s = document.getElementById('season').value
+    let ep = document.getElementById('ep').value
 
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-        console.log(data)
+            console.log(data)
+
+            let maxSeasons
+            function validateSeasonNum() {
+                let numSeasons = []
+                // create list of all season numbers
+                for (let obj in data) {
+                    numSeasons.push(data[obj].season)
+                }
+                // find highest number of seasons
+                maxSeasons = Math.max(...numSeasons)
+                // validate input
+                if (s <= 0 || s > maxSeasons) {
+                    alert(`Please enter a season number between 1 and ${maxSeasons}.`)
+                }
+            }
+            validateSeasonNum()
 
       })
 
